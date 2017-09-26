924b5150d680a7d78b03f2848a77623d00d15b23
Brittany Johnson <brittanytjohnson@icloud.com>
2017-09-26 13:17:02 -0400

    Added hashtag capabilities

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -1,11 +1,13 @@
 class EpicenterController < ApplicationController
 
   before_action :authenticate_user!
+
+  include TweetsHelper
   
   def feed
   	@following_tweets = [ ]
 
-  	Tweet.all.each do |tweet|
+  	Tweet.order(id: :desc).each do |tweet|
   		if current_user.following.include?(tweet.user_id) ||
   			current_user.id == tweet.user_id
   			
@@ -31,4 +33,30 @@ class EpicenterController < ApplicationController
 
     redirect_to show_user_path(id: params[:id])
   end
+
+  def epi_tweet
+    # @tweet = Tweet.new
+
+    # @tweet.message = "#{params[:tweet][:message]}"
+    # @tweet.user_id = "#{params[:tweet][:user_id].to_i}"
+
+    @tweet = Tweet.create(message: params[:tweet][:message], user_id: params[:tweet][:user_id].to_i)
+    
+    @tweet = get_tagged(@tweet)
+    @tweet.save
+
+    redirect_to root_path
+  end
+
+  def tag_tweets
+    @tag = Tag.find(params[:id])
+  end
+
+
 end
+
+
+
+
+
+
